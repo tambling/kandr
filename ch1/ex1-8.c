d0f0f0e542bd5a82a369ed8d290692fcbaeb5fec
Dean Tambling <dean.tambling@gmail.com>
2016-03-08 16:59:44 -0800

    Adds Exercise 1-8

---
+++ ch1/ex1-8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int spaces = 0;
+	char c;
+
+	while((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\t' || c == 'n')
+			spaces ++;
+	}
+
+	printf("%d spaces.\n", spaces);
+}
